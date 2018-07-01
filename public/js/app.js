b166b46b9c4855f5e485f074624e93e0fccdd601
Terence Pouke <pkterri@hotmail.com>
2018-07-01 07:21:46 +0100

    serive and pro page done

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,31 @@
-$(".action-nav a").click(function (e) {
-    e.preventDefault();
-    if ($(".pro-section").hasClass("active")) {
-        $(".pro-section").removeClass("active");
-        $(".service-section").addClass("active");
-    } else {
-        $(".pro-section").addClass("active");
-        $(".service-section").removeClass("active");
-    }
-})
+$(function () {
+    $(".action-nav a").click(function (e) {
+        e.preventDefault();
+        if ($(".pro-section").hasClass("active")) {
+            $(".pro-section").removeClass("active");
+            $(".service-section").addClass("active");
+        } else {
+            $(".pro-section").addClass("active");
+            $(".service-section").removeClass("active");
+        }
+    })
+
+    $(".input-video .content").click(function (e) {
+        e.preventDefault();
+        $(".input-video").addClass("uploaded");
+    })
+
+    $(".input-video .thumbnail .remove-vid").click(function (e) {
+        e.preventDefault();
+        $(".input-video").removeClass("uploaded");
+    })
+
+    $(".dark-modal .close-modal").click(function () {
+        $(".dark-modal").fadeOut();
+    })
+
+    $(".page-form").submit(function (e) {
+        e.preventDefault();
+        $(".dark-modal").fadeIn();
+    })
+});
